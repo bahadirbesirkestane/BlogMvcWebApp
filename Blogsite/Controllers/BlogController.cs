8506ae1ad4c4182862699080a8b350af5774270d
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-03 16:43:00 +0300

    UI controllerlar eklendi

---
+++ Blogsite/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Blogsite.BusinessLayer.Concrete;
+using Blogsite.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogsite.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+            var values = _blogManager.GetBlogListWithCategory();
+
+            return View(values);
+        }
+    }
+}
---
+++ Blogsite/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Blogsite.BusinessLayer.Concrete;
+using Blogsite.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogsite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var values=_categoryManager.GetAll();
+            return View(values);
+        }
+    }
+}
