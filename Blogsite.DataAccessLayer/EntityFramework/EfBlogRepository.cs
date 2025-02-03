70b5321fd2a9317e3244eb96eb9c6ca3af3143b0
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-03 16:40:56 +0300

    EF bağımlılıkları oluşturuldu

---
+++ Blogsite.DataAccessLayer/EntityFramework/EfBlogRepository.cs
@@ -0,0 +1,25 @@
+﻿using Blogsite.DataAccessLayer.Abstract;
+using Blogsite.DataAccessLayer.Concrete;
+using Blogsite.DataAccessLayer.Repositories;
+using Blogsite.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogsite.DataAccessLayer.EntityFramework
+{
+    public class EfBlogRepository : GenericRepository<Blog>, IBlogDAL
+    {
+        public List<Blog> GetListWithCategory()
+        {
+            using(var _context =new Context())
+            {
+                return _context.Blogs.Include(x => x.Category).ToList();
+            }
+            
+        }
+    }
+}
---
+++ Blogsite.DataAccessLayer/EntityFramework/EfCommentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Blogsite.DataAccessLayer.Abstract;
+using Blogsite.DataAccessLayer.Repositories;
+using Blogsite.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogsite.DataAccessLayer.EntityFramework
+{
+    public class EfCommentRepository : GenericRepository<Comment>, ICommentDAL
+    {
+    }
+}
