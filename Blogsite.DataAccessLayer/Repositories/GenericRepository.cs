034a433e210cf8779dedb5748d1602546b640e19
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-03 16:41:23 +0300

    Generic repository Oluşturuldu

---
+++ Blogsite.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Blogsite.DataAccessLayer.Abstract;
+using Blogsite.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogsite.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+        public void Delete(T entity)
+        {
+            using var _context = new Context();
+            _context.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            using var _context = new Context();
+            //var dene= _context.Set<T>().ToList();
+
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            using var _context = new Context();
+
+
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            using var _context = new Context();
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            using var _context = new Context();
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
