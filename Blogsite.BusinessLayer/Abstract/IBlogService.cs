854ccf536475104e80638b309ecf4988b529e441
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-03 16:42:05 +0300

    BusinessLayer Interfaceleri oluştuldu

---
+++ Blogsite.BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,19 @@
+﻿using Blogsite.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogsite.BusinessLayer.Abstract
+{
+    public interface IBlogService
+    {
+        void AddBlog(Blog blog);
+        void RemoveBlog(Blog blog);
+        void UpdateBlog(Blog blog);
+        List<Blog> GetAll();
+        Blog GetById(int id);
+        List<Blog> GetBlogListWithCategory();
+    }
+}
---
+++ Blogsite.BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using Blogsite.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogsite.BusinessLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        void AddCategory(Category category);
+        void RemoveCategory(Category category);
+        void UpdateCategory(Category category);
+        List<Category> GetAll();
+        Category GetById(int id);
+    }
+}
