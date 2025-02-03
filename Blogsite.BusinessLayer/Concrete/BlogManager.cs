88a1bb9ab9eba1cac1d197cbfb9fd6018c2b564e
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-03 16:42:34 +0300

    BusinesLayer Manager sınıfları oluşturuldu

---
+++ Blogsite.BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,51 @@
+﻿using Blogsite.BusinessLayer.Abstract;
+using Blogsite.DataAccessLayer.Abstract;
+using Blogsite.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogsite.BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDAL _blogDAL;
+
+        public BlogManager(IBlogDAL blogDAL)
+        {
+            _blogDAL = blogDAL;
+        }
+
+        public void AddBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetAll()
+        {
+            return _blogDAL.GetAll();
+        }
+
+        public List<Blog> GetBlogListWithCategory()
+        {
+            return _blogDAL.GetListWithCategory();
+        }
+
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Blogsite.BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using Blogsite.BusinessLayer.Abstract;
+using Blogsite.DataAccessLayer.Abstract;
+using Blogsite.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogsite.BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDAL _categoryDAL;
+
+        public CategoryManager(ICategoryDAL categoryDAL)
+        {
+            _categoryDAL = categoryDAL;
+        }
+        public void AddCategory(Category category)
+        {
+            _categoryDAL.Insert(category);
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDAL.GetAll();
+        }
+
+        public Category GetById(int id)
+        {
+            return _categoryDAL.GetById(id);
+        }
+
+        public void RemoveCategory(Category category)
+        {
+            _categoryDAL.Delete(category);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            _categoryDAL.Update(category);
+        }
+    }
+}
