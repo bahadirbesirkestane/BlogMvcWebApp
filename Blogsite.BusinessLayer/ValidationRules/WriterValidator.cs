b1c64d0a44da9b6b2b3000cd705c69d0f10d8d9d
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-05 22:38:08 +0300

    Validation Rules sınıfı oluşturuldu ve "Writer" için kurallar yazıldı.

---
+++ Blogsite.BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,25 @@
+﻿using Blogsite.EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogsite.BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı!");
+            
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez!");
+            
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilemez!");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("İsim en az 2 karakter olamlıdır!");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("İsim en fazla 50 karakter olamlıdır!");
+
+        }
+    }
+}
