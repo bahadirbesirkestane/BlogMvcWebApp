7d458a67d1c2e6a031e5b3547196ebd5896eabdf
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-06 17:32:29 +0300

    View Component eklendi

---
+++ Blogsite/ViewComponents/Blog/WriterLastBlogs.cs
@@ -0,0 +1,19 @@
+﻿using Blogsite.BusinessLayer.Concrete;
+using Blogsite.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogsite.ViewComponents.Blog
+{
+    public class WriterLastBlogs : ViewComponent
+    {
+        BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            int id=2;
+
+            var values = _blogManager.GetBlogListWithWriter(id);
+            return View(values);
+        }
+    }
+}
