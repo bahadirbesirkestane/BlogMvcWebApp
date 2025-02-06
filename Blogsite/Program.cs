86c426cc20d8268a6643714e1eb3fe452018c8a4
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-06 17:31:20 +0300

    Hata sayfası için yönlendirme ve sayfa düzenlemeleri yapıldı

--- Blogsite/Program.cs
+++ Blogsite/Program.cs
@@ -13,6 +13,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+//app.UseStatusCodePages();
+
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1","?code={0}");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
