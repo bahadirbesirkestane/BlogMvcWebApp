c775c9988d448d2c1356c4855bfe98c06ef594ae
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-15 17:34:25 +0300

    Dashboard eklendi

---
+++ Blogsite/Controllers/WriterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogsite.Controllers
+{
+    
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
